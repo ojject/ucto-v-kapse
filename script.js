3c975b8a5c015c1e359bb2f73e20bed373627bc3
ojject <69258173+ojject@users.noreply.github.com>
2025-09-09 21:00:48 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-// Year in footer
 document.getElementById('year').textContent = new Date().getFullYear();
-// Contact form feedback
 const form = document.querySelector('.contact');
 if (form){
   form.addEventListener('submit', ()=>{
